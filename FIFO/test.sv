c9988541eb5ac50eb4b69cc8a1d1e0f0e6348236
katikeBhavana <160314569+katikeBhavana@users.noreply.github.com>
2026-03-18 17:45:11 +0530

    Create test.sv

---
+++ FIFO/test.sv
@@ -0,0 +1,21 @@
+class test;
+
+environment env;
+virtual intf vif;
+
+function new(virtual intf vif);
+
+this.vif = vif;
+
+env = new(vif);
+
+endfunction
+
+
+task run();
+
+env.run();
+
+endtask
+
+endclass
